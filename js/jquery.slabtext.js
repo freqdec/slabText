1425b1c2cc2ed4f47a2b3a869cffbed83aa740da
Brian McAllister <frequency.decoder@gmail.com>
2012-01-08 13:58:16 +0100

    First commit

---
+++ js/jquery.slabtext.js
@@ -0,0 +1,166 @@
+/*! jQuery slabtext plugin v1 MIT/GPL2 @freqdec */
+(function( $ ){   
+
+        $.fn.slabText = function(options) {
+        
+                var settings = {
+                        // The ratio used when calculating the characters per line (parent width / (font-size * fontRatio))
+                        // You may wish to tweak this to suit your chosen font
+                        "fontRatio"             : 0.78,
+                        // Always recalculate the characters per line, not just when the font-size changes?
+                        // Defaults to true (CPU intensive)
+                        "forceNewCharCount"     : true,
+                        // Do we wrap ampersands in <span class="amp"> ?
+                        "wrapAmpersand"         : true,
+                        // Under what pixel width do we remove the slabtext styling?
+                        "headerBreakpoint"      : null,
+                        "viewportBreakpoint"    : null
+                };
+                
+                // Add the slabtexted classname to the body to initiate the styling of
+                // the injected spans
+                $("body").addClass("slabtexted");
+                
+                return this.each(function(){
+                   
+                        // Extend options if necessary
+                        if(options) { 
+                                $.extend(settings, options);
+                        };
+                        
+                        var $this               = $(this),                              
+                            keepSpans           = $("span.slabtext", $this).length,
+                            words               = keepSpans ? [] : String($this.text()).replace(/\s{2,}/g).split(" "),                              
+                            origFontSize        = null,                                 
+                            idealCharPerLine    = null,                                 
+                            fontRatio           = settings.fontRatio,                   
+                            forceNewCharCount   = settings.forceNewCharCount,
+                            headerBreakpoint    = settings.headerBreakpoint,
+                            viewportBreakpoint  = settings.viewportBreakpoint,
+                            resizeThrottle      = null,
+                            viewportWidth       = $(window).width();                                   
+                                                                   
+                        // Most of this function is a (very) stripped down AS3 to JS port of the slabtype
+                        // algorithm by Eric Loyer with the original comments left intact
+                        // http://erikloyer.com/index.php/blog/the_slabtype_algorithm_part_1_background/                         
+                        var resizeSlabs = function resizeSlabs() {
+                                
+                                // Cache the parent containers width       
+                                var parentWidth = $this.width();
+                                
+                                // Remove the slabtextdone and slabtextinactive classnames to enable the inline-block shrink-wrap effect
+                                $this.removeClass("slabtextdone slabtextinactive");
+                                
+                                if(viewportBreakpoint && viewportBreakpoint > viewportWidth
+                                   ||
+                                   headerBreakpoint && headerBreakpoint > parentWidth) {
+                                        // Add the slabtextinactive classname to set the spans as inline
+                                        $this.addClass("slabtextinactive");                                        
+                                        $("span.slabtext", $this).each(function() {
+                                                $(this).css('font-size', "inherit");                                                                                                     
+                                        });
+                                        return;
+                                };
+                                 
+                                // If the parent containers font-size has changed or the "forceNewCharCount" option is true (the default),
+                                // then recalculate the "characters per line" count and re-render the inner spans
+                                // Setting "forceNewCharCount" to false will save CPU cycles...                                                                                           
+                                if(!keepSpans && (forceNewCharCount || parseFloat($this.css('font-size')) != origFontSize)) {
+                                        
+                                        origFontSize = parseFloat($this.css('font-size'));                                        
+                                        
+                                        var newCharPerLine      = Math.min(60, Math.floor(parentWidth / (origFontSize * fontRatio))),
+                                            wordIndex           = 0,
+                                            lineText            = [],
+                                            counter             = 0,                                                                        
+                                            preText             = "",
+                                            postText            = "",
+                                            finalText           = "",
+                                            preDiff,
+                                            postDiff;
+                                
+                                        if(newCharPerLine != idealCharPerLine) {
+                                                idealCharPerLine = newCharPerLine;
+                                                                                        
+                                                while (wordIndex < words.length) {
+                                               
+                                                        postText = "";
+                            
+                                                        // build two strings (preText and postText) word by word, with one
+                                                        // string always one word behind the other, until
+                                                        // the length of one string is less than the ideal number of characters
+                                                        // per line, while the length of the other is greater than that ideal
+                                                        while (postText.length < idealCharPerLine) {
+                                                                preText   = postText;
+                                                                postText += words[wordIndex] + " ";
+                                                                if(++wordIndex >= words.length) {
+                                                                        break;
+                                                                };
+                                                        };
+                            
+                                                        // calculate the character difference between the two strings and the
+                                                        // ideal number of characters per line
+                                                        preDiff  = idealCharPerLine - preText.length;
+                                                        postDiff = postText.length - idealCharPerLine;
+                            
+                                                        // if the smaller string is closer to the length of the ideal than
+                                                        // the longer string, and doesn’t contain just a single character, then
+                                                        // use that one for the line
+                                                        if((preDiff < postDiff) && (preText.length > 2)) {
+                                                                finalText = preText;
+                                                                wordIndex--;              
+                                                        // otherwise, use the longer string for the line
+                                                        } else {
+                                                                finalText = postText;
+                                                        };
+                            
+                                                        lineText.push('<span class="slabtext">' + (settings.wrapAmpersand ? finalText.replace("&", '<span class="amp">&amp;</span>') : finalText) + "</span>");
+                                                };
+                                                
+                                                $this.html(lineText.join(""));
+                                        };        
+                                } else {
+                                        // We only need the font-size for the resize-to-fit functionality
+                                        // if not injecting the spans 
+                                        origFontSize = parseFloat($this.css('font-size'));
+                                };
+                                                                    
+                                // Loop through the spans changing font-size accordingly
+                                $("span.slabtext", $this).each(function() {
+                                        var $span    = $(this),
+                                            ratio    = parentWidth / $span.width(),
+                                            fontSize = parseFloat(this.style.fontSize) || origFontSize;
+                                                                                
+                                        $span.css('font-size', (fontSize * ratio) + "px");                                                                                                     
+                                });
+                                
+                                // Add the class slabtextdone to set a display:block on the child spans
+                                // and avoid styling & layout issues associated with inline-block
+                                $this.addClass("slabtextdone");
+                        };
+           
+                        // Immediate resize
+                        resizeSlabs();     
+                        
+                        // Window resize event          
+                        $(window).resize(function() {
+                                // Only run the resize code if the viewport width has changed.
+                                // we ignore the viewport height as it will be constantly changing 
+                                // due to the font-size resizing and IE fires a resize event whenever
+                                // vertical height has changed leading to an endless loop and
+                                // locked browser. Poor IE.
+                                
+                                // Could wrap this in conditional comments?
+                                if($(window).width() == viewportWidth) {
+                                        return;
+                                };
+                                
+                                viewportWidth = $(window).width();
+                                
+                                // Throttle the resize event to 300ms
+                                clearTimeout(resizeThrottle);
+                                resizeThrottle = setTimeout(resizeSlabs, 300);
+                        });        
+                });
+    };
+})(jQuery);
